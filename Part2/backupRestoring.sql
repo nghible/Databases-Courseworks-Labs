9891e4e70ee4cc3a3a3d93887a35cbeee89ad033
nghible <nghible@gmail.com>
2019-09-04 18:33:43 +0100

    Add files via upload

---
+++ Part2/backupRestoring.sql
@@ -0,0 +1,11 @@
+-- Part 1.1 backupRestoring.sql
+--
+-- Submitted by: Nghi Bao Le
+--
+
+
+-- add your Backup Restoring statements here
+
+-- this command is entered at the terminal
+
+SOURCE crimes2015.sql
